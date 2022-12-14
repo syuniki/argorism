6763ff9b5a7d58061cc4fb76d59d86acd93e9b1a
syuniki <s.tasaka91@gmail.com>
2022-12-14 21:49:51 +0900

    双方向リンクリスト

---
+++ linkedLists/doubly_linked_list.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+from typing import Any, Optional
+
+
+class Node:
+    def __init__(self, data: Any, next_node: Node = None, prev_node: Node = None) -> None:
+        self.data = data
+        self.next = next_node
+        self.prev = prev_node
+
+
+class DoublyLinkedList:
+    def __init__(self, head: Node = None) -> None:
+        self.head = head
+
+    def append(self, data: Any) -> None:
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = new_node
+        new_node.prev = current_node
+
+    def insert(self, data: Any) -> None:
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+
+        self.head.prev = new_node
+        new_node.next = self.head
+        self.head = new_node
+
+    def remove(self, data: Any) -> None:
+        current_node = self.head
+        # 先頭の削除
+        if current_node and current_node.data == data:
+            if current_node.next is None:
+                current_node = None
+                self.head = None
+            else:
+                next_node = current_node.next
+                next_node.prev = None
+                current_node = None
+                self.head = next_node
+
+        # 先頭以外の削除
+        while current_node and current_node.data != data:
+            current_node = current_node.next
+
+        if current_node is None:
+            return
+
+        if current_node.next is None:
+            prev = current_node.prev
+            prev.next = None
+            current_node = None
+            return
+        else:
+            next_node = current_node.next
+            prev_node = current_node.prev
+            prev_node.next = next_node
+            next_node.prev = prev_node
+            current_node = None
+            return
+
+    def print(self) -> Node:
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
+
+if __name__ == "__main__":
+    d = DoublyLinkedList()
+    d.append(1)
+    d.append(2)
+    d.append(3)
+    d.insert(0)
+    d.print()
+    print("######### Remove")
+    d.remove(3)
+    d.print()
