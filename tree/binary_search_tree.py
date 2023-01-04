bcd3cbcf31931552912a92cc0e78b1dc2a1d4226
syuniki <s.tasaka91@gmail.com>
2023-01-04 09:51:03 +0900

    inorder, search

--- tree/binary_search_tree.py
+++ tree/binary_search_tree.py
@@ -17,6 +17,26 @@ def insert(node: Node, value: int) -> Node:
     return node
 
 
+def inorder(node: Node) -> None:
+    # Inorder Left, Root, Right
+    if node is not None:
+        inorder(node.left)
+        print(node.value)
+        inorder(node.right)
+
+
+def search(node: Node, value: int) -> bool:
+    if node is None:
+        return False
+
+    if node.value == value:
+        return True
+    elif node.value > value:
+        return search(node.left, value)
+    else:
+        return search(node.right, value)
+
+
 if __name__ == "__main__":
     root = None
     root = insert(root, 3)
@@ -26,7 +46,5 @@ if __name__ == "__main__":
     root = insert(root, 1)
     root = insert(root, 10)
     root = insert(root, 2)
-    print(root.value)
-    print(root.right.value)
-    print(root.right.left.value)
-    print(root.left.value)
+    # inorder(root)
+    print(search(root, 5))
